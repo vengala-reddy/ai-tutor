68f4bdf5e33f5c2e209075af3255766b0ee18992
vengalareddy1998 <vengalareddy1998@gmail.com>
2025-09-21 06:51:05 +0000

    M13: add Two-way Binding

--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -2,16 +2,22 @@ import { Component, computed, signal } from '@angular/core';
 import { RecipeModel } from '../models';
 import { MOCK_RECIPES } from '../mock-recipes';
 import { RecipeDetail } from '../recipe-detail/recipe-detail';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [RecipeDetail],
+  imports: [RecipeDetail, FormsModule],
   templateUrl: './recipe-list.html',
   styleUrl: './recipe-list.css'
 })
 export class RecipeList {
   protected recipes:RecipeModel[] = MOCK_RECIPES;
   protected readonly recipe = signal<RecipeModel>(this.recipes[0]);   
+  protected readonly searchTerm = signal<string>('');
+  protected readonly filteredRecipes = computed(() => {
+    const search = this.searchTerm();
+    return this.recipes.filter(recipe => recipe.name.toLowerCase().includes(search.toLowerCase()));
+  });
 
   protected showRecipe(recipe:RecipeModel): void {
     console.log(`Show ${recipe} button clicked`);
